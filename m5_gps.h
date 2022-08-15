63914c4a659b0601dcaa84b822863ebfaeb413cc
gratingBoy <48827595+gratingBoy@users.noreply.github.com>
2022-08-15 22:26:43 +0900

    バッテリー残量取得 関数追加
    バッテリー残量を取得するための関数を追加した

--- m5_gps.h
+++ m5_gps.h
@@ -7,6 +7,7 @@
 #include <time.h>
 #include <WiFi.h>
 #include <driver/i2s.h>
+#include <SD.h>
 #include "AudioFileSourceSD.h"
 #include "AudioFileSourceID3.h"
 #include "AudioGeneratorMP3.h"
@@ -15,27 +16,27 @@
 //********************
 //  define
 //********************
-#define SERIAL_SPEED 9600     // GPSモジュール通信速度
-#define GPS_TX_PIN 13         // M5stack 送信ピンNo.
-#define GPS_RX_PIN 14         // M5stack 受信ピンNo.
-#define GPS_DELAY 100         // GPSループディレイ(ms)
-#define MAX_STRING 256        // 文字列最大長
-#define COLOR_DEPTH 8         // カラーモード(24bit)
-#define RED_COMP 0xc2         // 赤
-#define GREEN_COMP 0x89       // 緑
-#define BLUE_COMP 0x4b        // 青
-#define INIT_DELAY 2000       // イニシャル表示待ち
-#define ON 1                  // フラグON
-#define OFF 0                 // フラグOFF
-#define VERSION_NO "0.04"     // Version番号
-#define MP3_DELAY 1           // MP3再生待ちポーリング時間
-#define OUTPUT_GAIN 0.5       // MP3再生音量
-#define JST_OFFSET (9 * 3600) // UTC->JSTのオフセット
-#define I2S_NUM_0 0
-#define EXTERNAL_I2S 0
-#define BLCK_PIN 12
-#define WLCK_PIN 0
-#define DOUT_PIN 2
+#define SERIAL_SPEED 9600              // GPSモジュール通信速度
+#define GPS_TX_PIN 13                  // M5stack 送信ピンNo.
+#define GPS_RX_PIN 14                  // M5stack 受信ピンNo.
+#define GPS_DELAY 100                  // GPSループディレイ(ms)
+#define MAX_STRING 256                 // 文字列最大長
+#define COLOR_DEPTH 8                  // カラーモード(24bit)
+#define RED_COMP 0xc2                  // 赤
+#define GREEN_COMP 0x89                // 緑
+#define BLUE_COMP 0x4b                 // 青
+#define INIT_DELAY 2000                // イニシャル表示待ち
+#define ON 1                           // フラグON
+#define OFF 0                          // フラグOFF
+#define VERSION_NO "0.05"              // Version番号
+#define MP3_DELAY 1                    // MP3再生待ちポーリング時間
+#define OUTPUT_GAIN 0.5                // MP3再生音量
+#define JST_OFFSET (9 * 3600)          // UTC->JSTのオフセット
+#define I2S_NUM_0 0                    // 外部DAコンバータ設定
+#define EXTERNAL_I2S 0                 // 外部DAコンバータ設定
+#define BLCK_PIN 12                    // MP3 BLCK ピンNo.
+#define WLCK_PIN 0                     // MP3 WLCK ピンNo.
+#define DOUT_PIN 2                     // MP3出力ピンNo.
 #define YEAR_OFFSET 1900               // 年設定時のオフセット
 #define MONTH_OFFSET 1                 // 月設定時のオフセット
 #define MAX_STR 32                     // 文字列最大長
@@ -48,12 +49,12 @@
 #define FONT_24 &fonts::FreeMono24pt7b // フォント24pt
 //********** MP3再生タスク用 **********
 #define MP3_TASK_NAME "PlayVoiceTask" // タスク名
-#define MP3_STACK_SIZE 4096           // スタックエリアサイズ
+#define MP3_STACK_SIZE 8192           // スタックエリアサイズ
 #define MP3_TASK_PRIORITY 0           // タスク優先度
 #define MP3_CORE_ID 0                 // マルチタスク コアID
 //********** GSPタスク用 **********
 #define GPS_TASK_NAME "gpsTask" // タスク名
-#define GPS_STACK_SIZE 4096     // スタックエリアサイズ
+#define GPS_STACK_SIZE 8192     // スタックエリアサイズ
 #define GPS_TASK_PRIORITY 0     // タスク優先度
 #define GPS_CORE_ID 1           // マルチタスク コアID
 
@@ -65,20 +66,16 @@ static LGFX lcd;                 // LCD描画インスタンス
 static LGFX_Sprite sprite(&lcd); // スプライトエリアインスタンス
 static int initFlag = ON;        // イニシャルフラグ
 static int helloFlag = ON;       // あいさつ再生フラグ
-static AudioGeneratorMP3 *mp3;
-static AudioFileSourceSD *file;
-static AudioOutputI2S *out;
-static AudioFileSourceID3 *id3;
-static int recFlag = ON;
+static int recFlag = ON;         // 位置情報記録フラグ
 
 //********************
 //  プロトタイプ宣言
 //********************
 static void readGPSInfo(unsigned long ms);
 static void displayInfo();
-static void initDisp();
 static void initGPS();
 void playVoiceTask(void *arg);
 void gpsTask(void *arg);
 static void synchTime();
 static void recodingGPSInfo();
+static int getBatteryPersentage();
